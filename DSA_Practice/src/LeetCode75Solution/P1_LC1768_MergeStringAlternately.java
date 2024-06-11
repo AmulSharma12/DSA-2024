af3edc048243152295a1b71ae78ce175e5c56b4a
Amul Op <amulsharmarr67@gmail.com>
2024-06-11 23:34:41 +0530

    LeetCode 1768-Merge String Alternately

---
+++ DSA_Practice/src/LeetCode75Solution/P1_LC1768_MergeStringAlternately.java
@@ -0,0 +1,39 @@
+package LeetCode75Solution;
+import java.util.Scanner;
+public class P1_LC1768_MergeStringAlternately {
+    public static void main(String[] args){
+        System.out.println(mergeAlternately("abc", "pqr"));
+    }
+
+
+    private static String mergeAlternately(String word1, String word2) {
+        StringBuilder mergedString = new StringBuilder();
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+        int i = 0,j = 0;
+        boolean isWord1 = true;     //first letter is word1
+
+
+        //when there is letter in both the string word1 & word2
+        while(i < word1Length && j < word2Length){
+            if(isWord1){
+                mergedString.append(word1.charAt(i++));
+                isWord1 = false;    //if run for word1 then it turn to word2 vice versa
+            }else{
+                mergedString.append(word2.charAt(j++));
+                isWord1 = true;
+            }
+        }
+
+        //for remaining letters if exist in both the string
+        while(i < word1Length){
+            mergedString.append(word1.charAt(i++));
+        }
+
+        while(j < word2Length){
+            mergedString.append(word2.charAt(j++));
+        }
+
+        return mergedString.toString();
+    }
+}
