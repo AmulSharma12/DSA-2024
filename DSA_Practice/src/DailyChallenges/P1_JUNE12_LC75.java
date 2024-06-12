57050c8b107d075c5b998f73215765ae56ce4f44
Amul Op <amulsharmarr67@gmail.com>
2024-06-12 23:47:32 +0530

    LC-75 Approach 2

--- DSA_Practice/src/DailyChallenges/P1_JUNE12_LC75.java
+++ DSA_Practice/src/DailyChallenges/P1_JUNE12_LC75.java
@@ -15,6 +15,7 @@ public class P1_JUNE12_LC75 {
         }
 
         sortColors1(nums);
+        sortColors2(nums);
         System.out.println(Arrays.toString(nums));
     }
 
@@ -22,5 +23,24 @@ public class P1_JUNE12_LC75 {
     //Approach 1- using direct inbuilt function
     public static void sortColors1(int[] nums) {
        Arrays.sort(nums);
+    }
+
+
+    public static void sortColors2(int[] nums) {
+        int n = nums.length;
+        int count_0 = 0;
+        int count_1 = 0;
+        int count_2 = 0;
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] == 0)    count_0++;
+            else if(nums[i] == 1)   count_1++;
+            else count_2++;
+        }
+
+        int ind = 0;
+        while(count_0-- > 0)    nums[ind++] = 0;
+        while(count_1-- > 0)    nums[ind++] = 1;
+        while(count_2-- > 0)    nums[ind++] = 2;
     }
 }
