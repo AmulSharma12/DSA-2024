2b1d52bd120680463a086bfec32ffc104eb7edaf
Amul Op <amulsharmarr67@gmail.com>
2024-07-06 23:43:09 +0530

    lc-509-fibbonaci-approach2

--- DSA_Practice/src/Striver_Sheet/S4_Recursion/P2_LC_509_FibbonaciNumber.java
+++ DSA_Practice/src/Striver_Sheet/S4_Recursion/P2_LC_509_FibbonaciNumber.java
@@ -5,7 +5,7 @@ public class P2_LC_509_FibbonaciNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int nthFibbonaci = sc.nextInt();
-        System.out.println(fib(nthFibbonaci));
+        System.out.println(fib2(nthFibbonaci));
     }
 
     //Approach 1- using two pointer variables (naive)
@@ -24,5 +24,17 @@ public class P2_LC_509_FibbonaciNumber {
 
         //curr pointer variable will point to the updated nth fibbonaci number
         return curr;
+    }
+
+    //Approach 2 - using recursion
+    public static int fib2(int n) {
+        if(n == 0 || n == 1)
+            return n;
+
+        int prev = fib2(n-1);
+        int nextToPrev = fib2(n-2);
+
+        //now by getting both will sum up both
+        return prev + nextToPrev;
     }
 }
