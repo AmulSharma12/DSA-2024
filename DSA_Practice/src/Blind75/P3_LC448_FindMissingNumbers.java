5efbb80945a861e61a1cb3c84a0ad90cb97064c6
Amul Op <amulsharmarr67@gmail.com>
2024-08-03 23:25:32 +0530

    'lc-448-findDisappearedNumbers-approach2'

--- DSA_Practice/src/Blind75/P3_LC448_FindMissingNumbers.java
+++ DSA_Practice/src/Blind75/P3_LC448_FindMissingNumbers.java
@@ -40,4 +40,46 @@ public class P3_LC448_FindMissingNumbers {
 
         return missingElement;
     }
+
+
+    //Approach 2 - using constant space and O(N) time
+    public static List<Integer> findDisappearedNumbers2(int[] nums) {
+        int n = nums.length;
+        List<Integer> disappearedNumberList = new ArrayList<>();
+
+        //placing the element (nums[ind]) to their right position (element-1)
+        //[1...n] -> [0....n-1]
+        for(int ind = 0; ind < n; ){
+            int element = nums[ind];
+            int position = element  -1;
+            // System.out.println(element + " " + position);
+            if(nums[position] == element){
+                ind++;
+                continue;
+            }
+            swap(nums,ind, position);
+            // System.out.println(Arrays.toString(nums));
+        }
+
+
+
+        //scanning the updated nums array
+        //to find out the disappeared numberList
+        for(int ind = 0; ind < n; ind++){
+            if(ind+1 != nums[ind]){
+                disappearedNumberList.add(ind+1);
+            }
+        }
+
+
+
+        return disappearedNumberList;
+    }
+
+
+    private static void swap(int[] nums,int x, int y){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
 }
